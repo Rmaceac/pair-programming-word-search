ed2a37721b72542f14f50db7f4f4146bb27e338a
Ryan <ryan.maceachern@gmail.com>
2022-02-16 17:45:39 -0800

    basic functionality and tests complete

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,30 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+    // if provided array is empty, return false
+    if (letters.length === 0) {
+        return false;
     }
-}
+    // join the rows of letters in provided array into full words
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    // transpose the provided array and join the letter COLUMNS into full words
+    const verticalJoin = transpose(letters).map(ls => ls.join(''));
+    // loop through the array horizontally (rows)
+    for (const l of horizontalJoin) {
+        if (l.includes(word)) {
+            return true;
+        }
+    }
+    // loop through the array vertically (columns)
+    for (const l of verticalJoin) {
+        if (l.includes(word)) {
+            return true;
+        }
+    }
+    return false;
+};
 
-module.exports = wordSearch
+// function that transposes an array so that it can be scanned vertically.
+const transpose = function(matrix) {
+    return matrix[0].map((a, i) => matrix.map(b => b[i]));
+  };
+
+module.exports = wordSearch;
